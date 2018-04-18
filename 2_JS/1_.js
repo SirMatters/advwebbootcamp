d33f127828f384e68dbbd075acfd94eafe9f26c4
SirMatters <perov.krll@gmail.com>
2018-04-18 22:49:46 +0300

    forEach function implemented

--- 2_JS/1_.js
+++ 2_JS/1_.js
@@ -9,3 +9,23 @@ function higherOrger(fn) {
 }
 
 higherOrger(callback);
+
+// forEach implementation
+var arr = [1,2,3,4,5,6];
+     
+//forEach function is created using 
+//2 parameters
+ 
+function forEach(arr, callback){
+  for(var i = 0; i < arr.length; i++){
+    callback(arr[i], i, arr);
+  }
+}
+ 
+//Here I invoked forEach function with
+//a callback to multiply every array's
+//number by 2
+ 
+forEach(arr, function(number){
+  console.log(number*2);
+})
