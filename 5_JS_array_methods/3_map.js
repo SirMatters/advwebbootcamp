a62f2c150a85ba350cc70a8024d5db0f58bb4e25
SirMatters <perov.krll@gmail.com>
2018-04-22 23:02:15 +0300

    some refactor"

--- 5_JS_array_methods/3_map.js
+++ 5_JS_array_methods/3_map.js
@@ -18,11 +18,11 @@ function extractKey(arr, key){
       return ele.name;
     });
 }
-extractKey([{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}], 'name') // ['Elie', 'Tim', 'Matt', 'Colt']
+extractKey([{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}], 'name') 
 
 function extractFullName(arr){
   return arr.map(ele => {
     return [ele.first, ele.last].join(" ");
   })
 }
-extractFullName([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia"}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele"}]) // ['Elie Schoppik', 'Tim Garcia', 'Matt Lane', 'Colt Steele']
+extractFullName([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia"}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele"}])
