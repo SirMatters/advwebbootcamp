f8ab9118609666a075b1179465db2503bf2bf1a1
sirmatters <perov.krll@gmail.com>
2018-04-28 20:47:53 +0000

    routing bugs fixed

--- 8_node_intro/routes/todos.js
+++ 8_node_intro/routes/todos.js
@@ -5,11 +5,11 @@ var helpers = require("../helpers/todos")
 
 router.route('/')
   .get(helpers.getTodos)
-  .post(helpers.getTodos)
+  .post(helpers.createTodo)
 
 router.route('/:todoId')
-  .get(helpers.getTodo())
-  .put(helpers.updateTodo())
-  .delete(helpers.deleteTodo())
+  .get(helpers.getTodo)
+  .put(helpers.updateTodo)
+  .delete(helpers.deleteTodo)
 
 module.exports = router;
