952e04b0ce2641a42759375fd150915cc6aea619
sirmatters <perov.krll@gmail.com>
2018-05-02 22:44:29 +0000

    backend finished

--- 8_node_intro/todo_api/public/app.js
+++ 8_node_intro/todo_api/public/app.js
@@ -5,7 +5,50 @@ $(document).ready(function(){
     .catch(function(err){
       console.log(err);
     });
+  $('#todoInput').keypress(function(event){
+    if(event.which == 13 && $(this).val() != ''){
+      createTodo();
+    }
+  });
+  $('.list').on('click', 'span', function(e){ //specify on span
+    e.stopPropagation(); //not to react on li events
+    removeTodo($(this).parent());
+  });
+  $('.list').on('click','li', function(){
+    updateTodo($(this))
+  })
 });
+
+function updateTodo(todo){
+  var updateUrl = '/api/todos/'+ todo.data('todoId');
+  var isDone = !todo.data('completed'); //this data does not change after click
+  var updateData = {completed: isDone};
+  $.ajax({
+    method: 'PUT',
+    url: updateUrl,
+    data: updateData
+  })
+    .then(function(updatedTodo){
+      todo.toggleClass('done');
+      todo.data('completed', isDone); //update of item data
+      console.log(todo.data('completed'));
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+}
+
+function removeTodo(todo){
+  var clickedId = todo.data('todoId'); //to get data
+  todo.remove();
+  var deleteUrl = '/api/todos/'+ clickedId;
+  $.ajax({
+    method: 'DELETE',
+    url: deleteUrl
+  })
+    .then(function(data){console.log(data)})
+    .catch(function(err){console.log(err)});
+}
 
 function addTodos(todos) {
   todos.forEach(function(todo){
@@ -14,18 +57,15 @@ function addTodos(todos) {
 }
 
 function addTodo(todo){
-  var newTodo = $('<li class="task">'+todo.name+'</li>');
+  var newTodo = $('<li class="task">'+todo.name+' <span>X</span></li>');
+  newTodo.data('todoId', todo._id);
+  newTodo.data('completed', todo.completed);
   if (todo.completed) {
     newTodo.addClass('done');
   }
   $('.list').append(newTodo);
 }
 
-$('#todoInput').keypress(function(event){
-  if(event.which == 13){
-    createTodo();
-  }
-});
 
 function createTodo(){
   var userInput = $('#todoInput').val();
@@ -36,5 +76,3 @@ function createTodo(){
     })
     .catch(function(err){console.log(err);});
 }
-
-$('.task').on('click', function(){})
