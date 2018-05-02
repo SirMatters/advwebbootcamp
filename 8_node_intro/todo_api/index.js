d9658307f4b47f84022f49f011f0122e90da361a
sirmatters <perov.krll@gmail.com>
2018-05-02 13:17:19 +0000

    add front-end representation

--- 8_node_intro/todo_api/index.js
+++ 8_node_intro/todo_api/index.js
@@ -6,9 +6,11 @@ var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get("/", function(req, res){
-  res.json({message: "Hello from app file!"});
+  res.sendFile('index.html');
 });
 
 app.use('/api/todos', todoRoutes); //all the routes form todoRoutes will have this prefix: /api/todos
---
+++ 8_node_intro/todo_api/public/app.js
@@ -0,0 +1,40 @@
+/* global $ */
+$(document).ready(function(){
+  $.getJSON('/api/todos')
+    .then(addTodos)
+    .catch(function(err){
+      console.log(err);
+    });
+});
+
+function addTodos(todos) {
+  todos.forEach(function(todo){
+    addTodo(todo);
+  });
+}
+
+function addTodo(todo){
+  var newTodo = $('<li class="task">'+todo.name+'</li>');
+  if (todo.completed) {
+    newTodo.addClass('done');
+  }
+  $('.list').append(newTodo);
+}
+
+$('#todoInput').keypress(function(event){
+  if(event.which == 13){
+    createTodo();
+  }
+});
+
+function createTodo(){
+  var userInput = $('#todoInput').val();
+  $.post('/api/todos', {name: userInput})
+    .then(function(newTodo){
+      $('#todoInput').val('');
+      addTodo(newTodo);
+    })
+    .catch(function(err){console.log(err);});
+}
+
+$('.task').on('click', function(){})
