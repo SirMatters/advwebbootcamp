17e3dd8b795aaeda8304f10f06760984ebfec713
SirMatters <perov.krll@gmail.com>
2018-05-19 17:58:00 +0300

    Map and Set ex done

---
+++ 9_ES2015+/6_maps_sets.js
@@ -0,0 +1,48 @@
+class MessageBoard {  
+  constructor(){
+      this.messages = new Map;
+      this.id = 1;
+  }  
+  addMessage(str){
+    this.messages.set(this.id, str);
+    this.id++;
+    return this;
+  }
+  findMessageById(id){
+    return this.messages.get(id);
+  }
+  findMessageByValue(str){
+    for(let msg of this.map.values()){
+      if (msg == str) return msg
+    }			
+  }
+  removeMessage(id){
+    this.messages.delete(id);
+  }
+  numberOfMessages(){
+    return this.messages.size;
+  }
+  messagesToArray(){
+    return Array.from(this.messages.values());
+  }
+}
+
+function uniqueValues(arr){
+return new Set(arr);
+}
+
+function hasDuplicates(arr){
+return new Set(arr).size !== arr.length;
+}
+
+function countPairs(arr, num){
+let cache = new Set(arr);
+let count = 0;
+for(val of arr){
+  cache.delete(val);
+  if (cache.has(num-val)){
+    count++;
+  }
+}
+return count;
+}
