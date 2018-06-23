50645d4a6c36d4e85d48b940aaf209f349c55311
SirMatters <perov.krll@gmail.com>
2018-06-23 09:06:45 +0000

    Recipe app refactor

--- 11_React/react/recipe-props-starter/src/Recipe.js
+++ 11_React/react/recipe-props-starter/src/Recipe.js
@@ -8,12 +8,12 @@ class Recipe extends Component {
       <li key={index}>{ing}</li>
     ));
     return (
-      <div className="recipe-card">
+      <div className='recipe-card'>
         <div className='card-image'>
           <img src={img} alt={title} />
         </div>
         <div className='recipe-card-content'>
-          <h3 className='recipe-title'>Recipe {title}</h3>
+          <h3 className='recipe-title'>{title}</h3>
           <h4>Ingredients</h4>
           <ul>
             {ingredients}
