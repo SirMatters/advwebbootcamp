94734ec1f02bfc865bf8ceed15c7df26ae30aecc
Perov Kirill <kirill.perov@nokiantyres.com>
2018-09-30 19:30:11 +0300

    no delay implementation

--- 11_react/card_memory_game/src/MemoryGame.js
+++ 11_react/card_memory_game/src/MemoryGame.js
@@ -4,66 +4,64 @@ import Card from './Card'
 
 const colors = ['red', 'green', 'black', 'lightskyblue', 'blue', 'brown', 'magenta']
 
-const CardStates = {
-  HIDDEN: 0,
-  SHOWING : 1
-}
-
 class MemoryGame extends Component {
   constructor(props) {
     super(props);
 
     let cards = [
-      {id:0, showState: CardStates.HIDDEN, backgroundColor: colors[6]},
-      {id:1, showState: CardStates.HIDDEN, backgroundColor: colors[0]},
-      {id:2, showState: CardStates.HIDDEN, backgroundColor: colors[0]},
-      {id:3, showState: CardStates.HIDDEN, backgroundColor: colors[1]},
-      {id:4, showState: CardStates.HIDDEN, backgroundColor: colors[1]},
-      {id:5, showState: CardStates.HIDDEN, backgroundColor: colors[2]},
-      {id:6, showState: CardStates.HIDDEN, backgroundColor: colors[2]},
-      {id:7, showState: CardStates.HIDDEN, backgroundColor: colors[3]},
-      {id:8, showState: CardStates.HIDDEN, backgroundColor: colors[3]},
-      {id:9, showState: CardStates.HIDDEN, backgroundColor: colors[4]},
-      {id:10, showState: CardStates.HIDDEN, backgroundColor: colors[4]},
-      {id:13, showState: CardStates.HIDDEN, backgroundColor: colors[5]},
-      {id:11, showState: CardStates.HIDDEN, backgroundColor: colors[5]},
-      {id:12, showState: CardStates.HIDDEN, backgroundColor: colors[6]}
+      {id:0, showState: false, backgroundColor: colors[6]},
+      {id:1, showState: false, backgroundColor: colors[0]},
+      {id:2, showState: false, backgroundColor: colors[0]},
+      {id:3, showState: false, backgroundColor: colors[1]},
+      {id:4, showState: false, backgroundColor: colors[1]},
+      {id:5, showState: false, backgroundColor: colors[2]},
+      {id:6, showState: false, backgroundColor: colors[2]},
+      {id:7, showState: false, backgroundColor: colors[3]},
+      {id:8, showState: false, backgroundColor: colors[3]},
+      {id:9, showState: false, backgroundColor: colors[4]},
+      {id:10, showState: false, backgroundColor: colors[4]},
+      {id:13, showState: false, backgroundColor: colors[5]},
+      {id:11, showState: false, backgroundColor: colors[5]},
+      {id:12, showState: false, backgroundColor: colors[6]}
     ]
 
     cards = this.shuffle(cards);
-    this.state = {cards, chosen: ''};
+    this.state = {cards, chosen: '', noClick: false};
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(id){
-    let chosen ='';
-    let temp = '';
+    let chosen, temp, cards;
 
-    const changeCardState = (cards, id, newState) => {
-      return cards.map(c => {
-        if(c.id === id) {
-          temp = c.backgroundColor;
-          return({...c, showState: newState})
-        }
-        return c;
-      })
-    }
-
-    let cards = changeCardState(this.state.cards, id, CardStates.SHOWING)
-
-    if(this.state.chosen === '') {
-      chosen = temp;
-    } else {
-      if(temp === this.state.chosen) {
-        cards = cards.map(c => ({...c, showState: CardStates.SHOWING}))
-      } else {
-        cards = cards.map(c => ({...c, showState: CardStates.HIDDEN}));
-        chosen = '';
+    const toggleCardState = (cards, id) => {
+      if (true) {
+        return cards.map(c => {
+          if(c.id === id) {
+            temp = c.backgroundColor;
+            console.log(temp, !c.showState)
+            return({...c, showState: !c.showState})
+          }
+          return c;
+        })
       }
     }
 
-
+    if (this.state.cards.some(c => (id === c.id && c.showState === false))) {
+      cards = toggleCardState(this.state.cards, id)
+      if(this.state.chosen === '') {
+        chosen = temp;
+      } else {
+        if(temp === this.state.chosen) {
+          cards = cards.map(c => ({...c, showState: true}))
+        } else {
+          cards = cards.map(c => ({...c, showState: false}));
+          chosen = '';
+        }
+      }
+    } else {
+      cards = toggleCardState(this.state.cards, id)
+    }
     this.setState({cards, chosen});
   }
 
@@ -80,7 +78,7 @@ class MemoryGame extends Component {
     const cards = this.state.cards.map(c => (
       <Card
         key = {c.id}
-        showState = {c.showState !== CardStates.HIDDEN}
+        showState = {c.showState}
         backgroundColor = {c.backgroundColor}
         onClick={() => this.handleClick(c.id)}
       />
